55074a50d25123a09489e3be05ce72ac88e6158e
fran <ortizceballosf@gmail.com>
2026-02-18 12:41:33 -0300

    Acepta vercel

--- main.py
+++ main.py
@@ -93,7 +93,11 @@ def create_fastapi_app() -> FastAPI:
 
     fastapi_app.add_middleware(     
         CORSMiddleware,
-        allow_origins=["https://frontecommercefinal.vercel.app"],  
+        allow_origins=[
+        "https://goat-ecommerce-iota.vercel.app",
+        #"http://localhost:3000",
+        #"http://127.0.0.1:3000"
+        ],  
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
